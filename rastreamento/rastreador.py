8f7f27341483c4b840219a3c562476796ab2b7cb
HLAM <hemilioaraujo@gmail.com>
2020-08-10 19:28:59 -0300

    código refatorado

--- rastreamento/rastreador.py
+++ rastreamento/rastreador.py
@@ -1,35 +1,45 @@
 from selenium.webdriver import Firefox
 
-itens = 'PZ109998961BR'
-url_rastreador = 'https://www2.correios.com.br/sistemas/rastreamento/default.cfm'
-# itens = 'PZ153830489BR;PZ121976672BR;PZ109998961BR'
-browser = Firefox()
-# browser = Firefox(executable_path='/usr/local/bin')
-browser.implicitly_wait(10)
+class Rastreador:
+    def __init__(self):
+        self._url_rastreador = 'https://www2.correios.com.br/sistemas/rastreamento/default.cfm'
+        # itens = 'PZ153830489BR;PZ121976672BR;PZ109998961BR'
+        self._browser = Firefox()
+        # browser = Firefox(executable_path='/usr/local/bin')
+        self._browser.implicitly_wait(10)
 
+    def buscar_rastreamento(self, item):
+        form_objetos = 'objetos'
+        form_btn_buscar = 'btnPesq'
+        self._browser.get(self._url_rastreador)
+        input_objetos = self._browser.find_element_by_id(form_objetos)
+        input_btn_buscar = self._browser.find_element_by_id(form_btn_buscar)
+        input_objetos.send_keys(item)
+        input_btn_buscar.click()
 
-def buscar_rastreamento(itens):
-    form_objetos = 'objetos'
-    form_btn_buscar = 'btnPesq'
-    result_data_class = 'sroDtEvent'
-    result_conteudo_class = 'sroLbEvent'
-    browser.get(url_rastreador)
-    input_objetos = browser.find_element_by_id(form_objetos)
-    input_btn_buscar = browser.find_element_by_id(form_btn_buscar)
-    input_objetos.send_keys(itens)
-    input_btn_buscar.click()
+    def pegar_dados(self):
+        result_data_class = 'sroDtEvent'
+        result_conteudo_class = 'sroLbEvent'
+        data = self._browser.find_elements_by_class_name(result_data_class)
+        conteudo = self._browser.find_elements_by_class_name(result_conteudo_class)
+        data = self._browser.find_elements_by_class_name(result_data_class)
+        conteudo = self._browser.find_elements_by_class_name(result_conteudo_class)
 
-    data = browser.find_elements_by_class_name(result_data_class)
-    conteudo = browser.find_elements_by_class_name(result_conteudo_class)
+        return data, conteudo
 
-    # browser.quit()
+    def print_dados(self, data, conteudo):
+        for i in range(len(data)):
+            print(data[i].text)
+            print(conteudo[i].text)
 
-    return data, conteudo
+    def kill(self):
+        self._browser.quit()
 
 
 if __name__ == '__main__':
-    data, conteudo = buscar_rastreamento(itens)
-    for i in range(len(data)):
-        print(data[i].text)
-        print(conteudo[i].text)
-    browser.quit()
+    item = 'PZ109998961BR'
+    a = Rastreador()
+    a.buscar_rastreamento(item)
+    data, conteudo = a.pegar_dados()
+    a.print_dados(data, conteudo)
+    a.kill()
